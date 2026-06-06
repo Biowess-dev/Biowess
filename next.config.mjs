9176b3d1c10d3b7d54ec54c7f16bf2459ec7c6a4
biowess <biowesshq@gmail.com>
2026-06-06 17:00:35 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/biowess',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
